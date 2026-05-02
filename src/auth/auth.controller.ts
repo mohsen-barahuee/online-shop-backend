e9f4c52a32b61e926be516b9d31b767da18878b9
Mohsen barahuee <nakel3920@gmail.com>
2026-05-02 11:20:51 +0330

    make auth route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
-@Controller("auth")
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto) {
+    const { mobile, password, display_name } = registerDto;
+    return this.authService.register(mobile, password, display_name);
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const { mobile, password } = loginDto;
+    return this.authService.login(mobile, password);
+  }
+}
