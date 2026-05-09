2a0ff1a7e3b31d748a7e6888cedb51fef2083361
Mohsen barahuee <nakel3920@gmail.com>
2026-05-09 09:41:42 +0330

    remove update dto

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { Repository } from 'typeorm';
