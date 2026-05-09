9f825cd318d00e789d701f5cb9fa0d9d67d3cda5
Mohsen barahuee <nakel3920@gmail.com>
2026-05-09 09:37:17 +0330

    make service and controller for catgeroies

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,34 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res, HttpStatus } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import type { Response } from 'express';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoriesService.create(createCategoryDto);
+  async create(
+    @Res() res: Response,
+    @Body() createCategoryDto: CreateCategoryDto,
+  ) {
+    const category = await this.categoriesService.create(createCategoryDto);
+
+    return res.status(HttpStatus.CREATED).json({
+      statusCode: HttpStatus.CREATED,
+      data: category,
+      message: 'دسته بندی با موفقیت اضافه گردید',
+    });
   }
 
   @Get()
   findAll() {
     return this.categoriesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoriesService.update(+id, updateCategoryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriesService.remove(+id);
   }
 }
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,24 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  price: number;
+
+  @IsInt()
+  stock: number;
+
+  @IsOptional()
+  @IsArray()
+  categoryIds?: number[];
+}
